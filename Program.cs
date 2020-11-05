f9f6bba14e68be8d86be7fd10095c7260f316f2f
Jamie Myland <jamie.myland@gmail.com>
2020-11-05 09:49:30 +0000

    Initialised Dotnet Core project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace real_time_base
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
